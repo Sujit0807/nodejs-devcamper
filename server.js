349550c1c774175249d7acf3568c8bdb2741d04a
Sujit Mali <malisujit12@gmail.com>
2022-09-08 22:56:19 +0530

    Routes Created

--- server.js
+++ server.js
@@ -6,6 +6,35 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+app.get("/api/v1/bootcamps", (req, res, next) => {
+    res.status(200).json({ success: true, message: "show all bootcamps" });
+});
+
+app.get("/api/v1/bootcamps/:id", (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        message: `get bootcamp ${req.params.id}`,
+    });
+});
+
+app.post("/api/v1/bootcamps", (req, res, next) => {
+    res.status(200).json({ success: true, message: "create new bootcamp" });
+});
+
+app.put("/api/v1/bootcamps/:id", (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        message: `Update bootcamp ${req.params.id}`,
+    });
+});
+
+app.delete("/api/v1/bootcamps/:id", (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        message: `Delete bootcamp ${req.params.id}`,
+    });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(
