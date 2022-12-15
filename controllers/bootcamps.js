90f30a981e48a6e3252d6ad0b50c45a2bad342e5
Sujit Mali <malisujit12@gmail.com>
2022-12-15 11:48:51 +0530

    Create Bootcamp controller

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,50 +1,54 @@
-const Bootcamp = require("../models/Bootcamp");
+const Bootcamp = require('../models/Bootcamp');
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = (req, res, next) => {
-    res.status(200).json({ success: true, message: "show all bootcamps" });
+  res.status(200).json({ success: true, message: 'show all bootcamps' });
 };
 
 // @desc    Get all bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
 exports.getBootcamp = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        message: `get bootcamp ${req.params.id}`,
-    });
+  res.status(200).json({
+    success: true,
+    message: `get bootcamp ${req.params.id}`,
+  });
 };
 
 // @desc    Create new bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
 exports.createBootcamp = async (req, res, next) => {
+  try {
     const bootcamp = await Bootcamp.create(req.body);
 
     res.status(201).json({
-        success: true,
-        data: bootcamp,
+      success: true,
+      data: bootcamp,
     });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
 exports.updateBootcamp = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        message: `Update bootcamp ${req.params.id}`,
-    });
+  res.status(200).json({
+    success: true,
+    message: `Update bootcamp ${req.params.id}`,
+  });
 };
 
 // @desc    Delete bootcamp
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
 exports.deleteBootcamp = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        message: `Delete bootcamp ${req.params.id}`,
-    });
+  res.status(200).json({
+    success: true,
+    message: `Delete bootcamp ${req.params.id}`,
+  });
 };
